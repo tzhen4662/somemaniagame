b18a50af15bce57510d29f98351c45d7ca043951
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-02-01 13:09:52 -0500

    asd

--- Mania/src/thegame/startScreen.java
+++ Mania/src/thegame/startScreen.java
@@ -18,26 +18,39 @@ public class startScreen extends Application
 		{
 			Button button = new Button();
 			button.setText("'Start'");
+			button.setStyle("-fx-border-color: #0000ff; -fx-background-color: #00BFFF; -fx-font-size: 1.5em");
 			Button button2 = new Button();
 			button2.setText("'Highscores'");
+			button2.setStyle("-fx-border-color: #0000ff; -fx-background-color: #87CEFA; -fx-font-size: 1.5em");
 			Button button3 = new Button();
 			button3.setText("'Controls'");
+			button3.setStyle("-fx-border-color: #0000ff; -fx-background-color: #87CEEB; -fx-font-size: 1.5em");
 			
-			button.setMaxSize(500, 70);
+			/*button.setMaxSize(500, 70);
 			button2.setMaxSize(500, 70);
-			button3.setMaxSize(500, 70);
+			button3.setMaxSize(500, 70);*/
 			
+			button.setOnAction(value ->  {
+	        	System.out.println("Starting ");
+			});
+			button2.setOnAction(value ->  {
+	        	System.out.println("No Highscores ");
+			});
+			button3.setOnAction(value ->  {
+	        	System.out.println("Controls ");
+			});
+	       
 			Pane root = new Pane();
 		    root.getChildren().add(button);
 		    root.getChildren().add(button2);
 		    root.getChildren().add(button3);
-			button.setLayoutX(450);
-			button.setLayoutY(400);
-			button2.setLayoutX(450);
-			button2.setLayoutY(450);
-			button3.setLayoutX(450);
-			button3.setLayoutY(500);
-		    Scene scene = new Scene(root, 1000, 1000);
+			button.setLayoutX(250);
+			button.setLayoutY(200);
+			button2.setLayoutX(225);
+			button2.setLayoutY(250);
+			button3.setLayoutX(235);
+			button3.setLayoutY(300);
+		    Scene scene = new Scene(root, 525, 550);
 		    primaryStage.setScene(scene);
 		    primaryStage.show();
 		    
