d3d3d7d90f51473091a4bac7dc760a31c1d5e724
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-02-01 13:11:32 -0500

    reeeeeeeeee

--- Mania/src/thegame/fallingNotes.java
+++ Mania/src/thegame/fallingNotes.java
@@ -18,6 +18,8 @@ public class fallingNotes extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		
+		double n = r.nextDouble() + 2;
+			
 		Circle circle1 = new Circle();
 		circle1.setCenterX(100.0);
 		circle1.setCenterY(100.0);
@@ -72,50 +74,68 @@ public class fallingNotes extends Application{
 		line5.setStartY(-400.0);
 		line5.setEndX(300.0);
 		line5.setEndY(600.0);
-		
-		for (int i = 0; i < 10; i++)
-		{
-			double n = r.nextDouble() + 2;
-			double n2 = r.nextDouble() + 2;
-			double n3 = r.nextDouble() + 2;
-			double n4 = r.nextDouble() + 2;
-			double n5 = r.nextDouble() + 2;
 			
-			PathTransition transition1 = new PathTransition();
-			transition1.setNode(circle1);
-			transition1.setDuration(Duration.seconds(n));
-			transition1.setPath(line1);
-			transition1.setCycleCount(transition1.INDEFINITE ); //transition1.INDEFINITE 
-			transition1.play();
+		PathTransition transition1 = new PathTransition();
+		transition1.setNode(circle1);
+		transition1.setDuration(Duration.seconds(n));
+		transition1.setPath(line1);
+		transition1.setCycleCount(1); //transition1.INDEFINITE 
+		transition1.play();
 		
-			PathTransition transition2 = new PathTransition();
-			transition2.setNode(circle2);
-			transition2.setDuration(Duration.seconds(n2));
-			transition2.setPath(line2);
-			transition2.setCycleCount(transition1.INDEFINITE );
-			transition2.play();
+		PathTransition transition2 = new PathTransition();
+		transition2.setNode(circle2);
+		transition2.setDuration(Duration.seconds(n));
+		transition2.setPath(line2);
+		transition2.setCycleCount(1);
+		transition2.play();
 		
-			PathTransition transition3 = new PathTransition();
-			transition3.setNode(circle3);
-			transition3.setDuration(Duration.seconds(n3));
-			transition3.setPath(line3);
-			transition3.setCycleCount(transition1.INDEFINITE );
-			transition3.play();
+		PathTransition transition3 = new PathTransition();
+		transition3.setNode(circle3);
+		transition3.setDuration(Duration.seconds(n));
+		transition3.setPath(line3);
+		transition3.setCycleCount(1);
+		transition3.play();
 		
-			PathTransition transition4 = new PathTransition();
-			transition4.setNode(circle4);
-			transition4.setDuration(Duration.seconds(n4));
-			transition4.setPath(line4);
-			transition4.setCycleCount(transition1.INDEFINITE );
-			transition4.play();
+		PathTransition transition4 = new PathTransition();
+		transition4.setNode(circle4);
+		transition4.setDuration(Duration.seconds(n));
+		transition4.setPath(line4);
+		transition4.setCycleCount(1);
+		transition4.play();
 		
-			PathTransition transition5 = new PathTransition();
-			transition5.setNode(circle5);
-			transition5.setDuration(Duration.seconds(n5));
-			transition5.setPath(line5);
-			transition5.setCycleCount(transition1.INDEFINITE );
-			transition5.play();
-		}
+		PathTransition transition5 = new PathTransition();
+		transition5.setNode(circle5);
+		transition5.setDuration(Duration.seconds(n));
+		transition5.setPath(line5);
+		transition5.setCycleCount(1);
+		transition5.play();
+		
+		//These are the clicker circles at the bottom
+		Circle clickCircle1 = new Circle();
+		clickCircle1.setCenterX(0.0);
+		clickCircle1.setCenterY(600.0);
+		clickCircle1.setRadius(50.0);
+		
+		Circle clickCircle2 = new Circle();
+		clickCircle2.setCenterX(100.0);
+		clickCircle2.setCenterY(600.0);
+		clickCircle2.setRadius(50.0);
+		
+		Circle clickCircle3 = new Circle();
+		clickCircle3.setCenterX(200.0);
+		clickCircle3.setCenterY(600.0);
+		clickCircle3.setRadius(50.0);
+
+		
+		Circle clickCircle4 = new Circle();
+		clickCircle4.setCenterX(300.0);
+		clickCircle4.setCenterY(600.0);
+		clickCircle4.setRadius(50.0);
+		
+		Circle clickCircle5 = new Circle();
+		clickCircle5.setCenterX(400.0);
+		clickCircle5.setCenterY(600.0);
+		clickCircle5.setRadius(50.0);
 		
 		StackPane root = new StackPane();
         root.getChildren().add(circle1);
@@ -123,7 +143,12 @@ public class fallingNotes extends Application{
         root.getChildren().add(circle3);
         root.getChildren().add(circle4);
         root.getChildren().add(circle5);
-        Scene scene = new Scene(root, 400, 400);
+        root.getChildren().add(clickCircle1);
+        //root.getChildren().add(clickCircle2);
+       // root.getChildren().add(clickCircle3);
+        //root.getChildren().add(clickCircle4);
+       // root.getChildren().add(clickCircle5);
+        Scene scene = new Scene(root, 1000, 1000);
         stage.setScene(scene);
         stage.show();
 	}
