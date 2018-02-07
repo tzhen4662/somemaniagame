26c40d8056d9cedd74d15e4c0446e3039fbd60f4
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-02-07 12:32:29 -0500

    Back button

--- Mania/src/thegame/startScreen.java
+++ Mania/src/thegame/startScreen.java
@@ -22,9 +22,9 @@ public class startScreen extends Application
 			Label Title = new Label();
 			Title.setText("Game Title");
 			Title.setStyle("-fx-background-color: #FFFFFF; -fx-font-size: 3em;	-fx-padding: 8 8 8 8; -fx-text-fill: #0000CC; -fx-border-width: 2; -fx-border-radius: 30; -fx-background-color: transparent; -fx-border-color: #99CCFF");
-			Label Title2 = new Label();
-			Title2.setText("Controls");
-			Title2.setStyle("-fx-background-color: #FFFFFF; -fx-font-size: 3em;	-fx-padding: 8 8 8 8; -fx-text-fill: #0000CC; -fx-border-width: 2; -fx-border-radius: 30; -fx-background-color: transparent; -fx-border-color: #99CCFF");
+			Label controlTitle = new Label();
+			controlTitle.setText("Controls");
+			controlTitle.setStyle("-fx-background-color: #FFFFFF; -fx-font-size: 3em;	-fx-padding: 8 8 8 8; -fx-text-fill: #0000CC; -fx-border-width: 2; -fx-border-radius: 30; -fx-background-color: transparent; -fx-border-color: #99CCFF");
 			Button button = new Button();
 			button.setText("Start");
 			button.setStyle("-fx-border-color: #0000ff; -fx-background-color: #00BFFF; -fx-font-size: 20px; -fx-text-fill: white;");
@@ -42,22 +42,26 @@ public class startScreen extends Application
 			button4.setStyle("-fx-border-color: #0000ff; -fx-background-color: #00BFFF; -fx-font-size: 1.5em; -fx-text-fill: white;");
 			Button endButton = new Button();
 			endButton.setText("End Game");
-			endButton.setStyle("-fx-border-color: #99CCFF; -fx-background-color: #CCCCFF; -fx-font-size: 10px; -fx-text-fill: white;");
+			endButton.setStyle("-fx-border-color: #99CCFF; -fx-background-color: #CCCCFF; -fx-font-size: 15px; -fx-text-fill: white;");
+			Button BackButton = new Button();
+			BackButton.setText("Back");
+			BackButton.setStyle("-fx-border-color: #99CCFF; -fx-background-color: #CCCCFF; -fx-font-size: 15px; -fx-text-fill: white;");
 
 		    //Buttons
 			Pane root = new Pane();
 			Pane root2 = new Pane();
 			root.getChildren().add(Title);
-			root2.getChildren().add(Title2);
+			root2.getChildren().add(controlTitle);
 		    root.getChildren().add(button);
 		    root.getChildren().add(button2);
 		    root.getChildren().add(button3);
 		    root2.getChildren().add(button4);
 		    root.getChildren().add(endButton);
+		    root2.getChildren().add(BackButton);
 		    Title.setLayoutX(210);
 			Title.setLayoutY(140);
-		    Title2.setLayoutX(220);
-			Title2.setLayoutY(140);
+		    controlTitle.setLayoutX(220);
+			controlTitle.setLayoutY(140);
 			button.setLayoutX(270);
 			button.setLayoutY(250);
 			button2.setLayoutX(240);
@@ -68,8 +72,10 @@ public class startScreen extends Application
 			button4.setLayoutY(240);
 			endButton.setLayoutX(0);
 			endButton.setLayoutY(0);
+			BackButton.setLayoutX(0);
+			BackButton.setLayoutY(0);
 		    Scene scene = new Scene(root, 600, 600);
-		    Scene Control = new Scene(root2, 600, 600);
+		    Scene control = new Scene(root2, 600, 600);
 		    primaryStage.setScene(scene);
 		    primaryStage.show();
 		    
@@ -82,11 +88,15 @@ public class startScreen extends Application
 			});
 			button3.setOnAction(value ->  {
 				//System.out.println("Controls ");
-				primaryStage.setScene(Control)
+				primaryStage.setScene(control)
 			;});
 			button4.setOnAction(value ->  {
 	        	System.out.println("Controls ");
 			});
 		    endButton.setOnAction(e -> Platform.exit());
+			BackButton.setOnAction(value ->  {
+				//System.out.println("Controls ");
+				primaryStage.setScene(scene)
+			;});
 		}
 }
