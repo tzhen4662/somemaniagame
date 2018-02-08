014450856b70333f5c24ab6d0ce9d8d0928874bf
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-02-08 13:08:01 -0500

    ok

--- Mania/src/thegame/fallingNotes.java
+++ Mania/src/thegame/fallingNotes.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.animation.PathTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
@@ -23,33 +26,35 @@ import javafx.util.Duration;
 public class fallingNotes extends Application{
 
 	private Random r = new Random();
-	private Circle circle1 = new Circle();
-	private Circle circle2 = new Circle();
-	private Circle circle3 = new Circle();
-	private Circle circle4 = new Circle();
-	private Circle circle5 = new Circle();
+	double rand = Math.random() + 1;
+	
+	private Circle circle1 = new Circle(100, 0, 20);
+	private Circle circle2 = new Circle(200, 0, 20);
+	private Circle circle3 = new Circle(300, 0, 20);
+	private Circle circle4 = new Circle(400, 0, 20);
+	private Circle circle5 = new Circle(500, 0, 20);
 	private Circle clickCircle1 = new Circle(100, 590, 20);
 	private Circle clickCircle2 = new Circle(200, 590, 20);
 	private Circle clickCircle3 = new Circle(300, 590, 20);
 	private Circle clickCircle4 = new Circle(400, 590, 20);
 	private Circle clickCircle5 = new Circle(500, 590, 20);
-	/*
-	private Line line1 = new Line();
-	private Line line2 = new Line();
-	private Line line3 = new Line();
-	private Line line4 = new Line();
-	private Line line5 = new Line();
-	private PathTransition transition1 = new PathTransition();
-	private PathTransition transition2 = new PathTransition();
-	private PathTransition transition3 = new PathTransition();
-	private PathTransition transition4 = new PathTransition();
-	private PathTransition transition5 = new PathTransition();
-	*/
+	
+	TranslateTransition transition1 = new TranslateTransition(Duration.seconds(2), circle1);
+	TranslateTransition transition2 = new TranslateTransition(Duration.seconds(2), circle2);
+	TranslateTransition transition3 = new TranslateTransition(Duration.seconds(2), circle3);
+	TranslateTransition transition4 = new TranslateTransition(Duration.seconds(2), circle4);
+	TranslateTransition transition5 = new TranslateTransition(Duration.seconds(2), circle5);
+	
+	PauseTransition pt1 = new PauseTransition(Duration.seconds(rand1));
+	
+	SequentialTransition seq1 = new SequentialTransition (circle1, pt1, transition1);
+	SequentialTransition seq2 = new SequentialTransition (circle2, pt2, transition2);
+	SequentialTransition seq3 = new SequentialTransition (circle3, pt3, transition3);
+	SequentialTransition seq4 = new SequentialTransition (circle4, pt4, transition4);
+	SequentialTransition seq5 = new SequentialTransition (circle5, pt5, transition5);
+	
 	private int score = 0;
 	Text scenetitle = new Text("Score");
-	long timeStep = System.nanoTime() + 10000000000L;
-	private String[] xValues = {"100","200","300","400","500"};
-	private int x = 0;
 	
 	
 	@Override
@@ -63,86 +68,34 @@ public class fallingNotes extends Application{
 		circle4.setRadius(20.0);
 		circle5.setRadius(20.0);
 		
-		/*
-		line1.setStartX(100.0);
-		line1.setStartY(-400.0);
-		line1.setEndX(100.0);
-		line1.setEndY(590.0);
+		transition1.setByY(590);
+		transition1.setCycleCount(2);
 		
-		line2.setStartX(200.0);
-		line2.setStartY(-400.0);
-		line2.setEndX(200.0);
-		line2.setEndY(590.0);
+		transition2.setByY(590);
+		transition2.setCycleCount(2);
+
+		transition3.setByY(590);
+		transition3.setCycleCount(2);
+
+		transition4.setByY(590);
+		transition4.setCycleCount(2);
+
+		transition5.setByY(590);
+		transition5.setCycleCount(2);
 		
-		line3.setStartX(300.0);
-		line3.setStartY(-400.0);
-		line3.setEndX(300.0);
-		line3.setEndY(590.0);
-		
-		line4.setStartX(400.0);
-		line4.setStartY(-400.0);
-		line4.setEndX(400.0);
-		line4.setEndY(590.0);
-		
-		line5.setStartX(500.0);
-		line5.setStartY(-400.0);
-		line5.setEndX(500.0);
-		line5.setEndY(590.0);
-			
-		transition1.setNode(circle1);
-		transition1.setDuration(Duration.seconds(n1));
-		transition1.setPath(line1);
-		transition1.setCycleCount(5); //transition1.INDEFINITE 
-		transition1.play();
-		
-		transition2.setNode(circle2);
-		transition2.setDuration(Duration.seconds(n2));
-		transition2.setPath(line2);
-		transition2.setCycleCount(5);
-		transition2.play();
-		
-		transition3.setNode(circle3);
-		transition3.setDuration(Duration.seconds(n3));
-		transition3.setPath(line3);
-		transition3.setCycleCount(5);
-		transition3.play();
-		
-		transition4.setNode(circle4);
-		transition4.setDuration(Duration.seconds(n4));
-		transition4.setPath(line4);
-		transition4.setCycleCount(5);
-		transition4.play();
-		
-		transition5.setNode(circle5);
-		transition5.setDuration(Duration.seconds(n5));
-		transition5.setPath(line5);
-		transition5.setCycleCount(5);
-		transition5.play();
-		*/
-		
-		new AnimationTimer() {
-	        public void handle(long now) {   
-	        	int y = 0;
-	          for (int i = 0; i < xValues.length; i++)
-		      {
-		        	x = Integer.parseInt(xValues[i]);
-		      }
-	          Circle fallingCircle = new Circle(x, 0, 20);
-	          while (now > timeStep)
-	          {
-	        	  fallingCircle.setCenterY(y = y + 100);
-	          }
-	        }
-	    }.start();
-	    
+		seq1.play();
+		seq2.play();
+		seq3.play();
+		seq4.play();
+		seq5.play();
 		
 		scenetitle.setLayoutX(300);
 		scenetitle.setLayoutY(300);
 		
 		Pane root = new Pane();
-		//root.getChildren().addAll(clickCircle1, clickCircle2, clickCircle3, clickCircle4, clickCircle5);
+		root.getChildren().addAll(clickCircle1, clickCircle2, clickCircle3, clickCircle4, clickCircle5);
 		root.getChildren().add(scenetitle);
-        //root.getChildren().addAll(circle1, circle2, circle3, circle4, circle5);
+        root.getChildren().addAll(circle1, circle2, circle3, circle4, circle5);
         Scene scene = new Scene(root, 600, 600);
         stage.setResizable(false);
         stage.setTitle("REEEEEEEEEEE");
@@ -153,24 +106,40 @@ public class fallingNotes extends Application{
             @Override
             public void handle(KeyEvent event) {
                 if (event.getCode() == KeyCode.SPACE){
-                	scenetitle.setText("" + score + "");
-                	score++;
+                	if (circle3.getCenterY() >= 560)
+                	{
+                		scenetitle.setText("" + score + "");   
+                		score = score + 100;
+                	}
+                	score = score + 0;
                 }          
                 if(event.getCode() == KeyCode.D) {
-                	scenetitle.setText("" + score + "");
-                	score++;
+                	if (circle1.getCenterY() >= 560)
+                	{
+                		scenetitle.setText("" + score + "");   
+                		score = score + 100;
+                	}
                 }
                 if(event.getCode() == KeyCode.F) {
-                	scenetitle.setText("" + score + "");
-                	score++;
+                	if (circle2.getCenterY() >= 560)
+                	{
+                		scenetitle.setText("" + score + "");   
+                		score = score + 100;
+                	}
                 }
                 if(event.getCode() == KeyCode.J) {
-                	scenetitle.setText("" + score + "");
-                	score++;
+                	if (circle4.getCenterY() >= 560)
+                	{
+                		scenetitle.setText("" + score + "");   
+                		score = score + 100;
+                	}
                 }
                 if(event.getCode() == KeyCode.K) {      
-                	scenetitle.setText("" + score + "");
-                	score++;
+                	if (circle5.getCenterY() >= 560)
+                	{
+                		scenetitle.setText("" + score + "");   
+                		score = score + 100;
+                	}
                 }
             }
         });
