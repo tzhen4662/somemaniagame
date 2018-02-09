894cb8faa5bf7a8a3eea186509bc466e142c402f
Tim <Tim@DESKTOP-O9RF4TE.fios-router.home>
2018-02-08 22:13:58 -0500

    still kind of doesnt work yet

--- Mania/src/thegame/fallingNotes.java
+++ Mania/src/thegame/fallingNotes.java
@@ -3,11 +3,16 @@ package thegame;
 import java.util.Random;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.PathTransition;
 import javafx.animation.PauseTransition;
 import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
 import javafx.scene.Scene;
@@ -28,30 +33,16 @@ public class fallingNotes extends Application{
 	private Random r = new Random();
 	double rand = Math.random() + 1;
 	
-	private Circle circle1 = new Circle(100, 0, 20);
-	private Circle circle2 = new Circle(200, 0, 20);
-	private Circle circle3 = new Circle(300, 0, 20);
-	private Circle circle4 = new Circle(400, 0, 20);
-	private Circle circle5 = new Circle(500, 0, 20);
+	private Circle circle1 = new Circle(100, -50, 20);
+	private Circle circle2 = new Circle(200, -50, 20);
+	private Circle circle3 = new Circle(300, -50, 20);
+	private Circle circle4 = new Circle(400, -50, 20);
+	private Circle circle5 = new Circle(500, -50, 20);
 	private Circle clickCircle1 = new Circle(100, 590, 20);
 	private Circle clickCircle2 = new Circle(200, 590, 20);
 	private Circle clickCircle3 = new Circle(300, 590, 20);
 	private Circle clickCircle4 = new Circle(400, 590, 20);
 	private Circle clickCircle5 = new Circle(500, 590, 20);
-	
-	TranslateTransition transition1 = new TranslateTransition(Duration.seconds(2), circle1);
-	TranslateTransition transition2 = new TranslateTransition(Duration.seconds(2), circle2);
-	TranslateTransition transition3 = new TranslateTransition(Duration.seconds(2), circle3);
-	TranslateTransition transition4 = new TranslateTransition(Duration.seconds(2), circle4);
-	TranslateTransition transition5 = new TranslateTransition(Duration.seconds(2), circle5);
-	
-	PauseTransition pt1 = new PauseTransition(Duration.seconds(rand1));
-	
-	SequentialTransition seq1 = new SequentialTransition (circle1, pt1, transition1);
-	SequentialTransition seq2 = new SequentialTransition (circle2, pt2, transition2);
-	SequentialTransition seq3 = new SequentialTransition (circle3, pt3, transition3);
-	SequentialTransition seq4 = new SequentialTransition (circle4, pt4, transition4);
-	SequentialTransition seq5 = new SequentialTransition (circle5, pt5, transition5);
 	
 	private int score = 0;
 	Text scenetitle = new Text("Score");
@@ -67,32 +58,58 @@ public class fallingNotes extends Application{
 		circle3.setRadius(20.0);
 		circle4.setRadius(20.0);
 		circle5.setRadius(20.0);
-		
-		transition1.setByY(590);
+	
+		/*transition1.setToY(640);
 		transition1.setCycleCount(2);
-		
-		transition2.setByY(590);
+
+		transition2.setToY(640);
 		transition2.setCycleCount(2);
 
-		transition3.setByY(590);
+		transition3.setToY(640);
 		transition3.setCycleCount(2);
 
-		transition4.setByY(590);
+		transition4.setToY(640);
 		transition4.setCycleCount(2);
 
-		transition5.setByY(590);
+		transition5.setToY(640);
 		transition5.setCycleCount(2);
+
+		transition1.play();
+		transition2.play();
+		transition3.play();
+		transition4.play();
+		transition5.play();*/
 		
-		seq1.play();
-		seq2.play();
-		seq3.play();
-		seq4.play();
-		seq5.play();
+		Timeline timeline = new Timeline();
+
+        KeyFrame kf1 = new KeyFrame(Duration.seconds(1),
+                new KeyValue(circle1.centerXProperty(), 100),
+                new KeyValue(circle1.centerYProperty(), 590));
+        KeyFrame kf2 = new KeyFrame(Duration.seconds(1),
+                new KeyValue(circle2.centerXProperty(), 200),
+                new KeyValue(circle2.centerYProperty(), 590));
+        KeyFrame kf3 = new KeyFrame(Duration.seconds(1),
+                new KeyValue(circle3.centerXProperty(), 300),
+                new KeyValue(circle3.centerYProperty(), 590));
+        KeyFrame kf4 = new KeyFrame(Duration.seconds(1),
+                new KeyValue(circle4.centerXProperty(), 400),
+                new KeyValue(circle4.centerYProperty(), 590));
+        KeyFrame kf5 = new KeyFrame(Duration.seconds(1),
+                new KeyValue(circle5.centerXProperty(), 500),
+                new KeyValue(circle5.centerYProperty(), 590));
+
+        timeline.getKeyFrames().add(kf1);
+        timeline.getKeyFrames().add(kf2);
+        timeline.getKeyFrames().add(kf3);
+        timeline.getKeyFrames().add(kf4);
+        timeline.getKeyFrames().add(kf5);
 		
+        
 		scenetitle.setLayoutX(300);
 		scenetitle.setLayoutY(300);
 		
 		Pane root = new Pane();
+		timeline.play();
 		root.getChildren().addAll(clickCircle1, clickCircle2, clickCircle3, clickCircle4, clickCircle5);
 		root.getChildren().add(scenetitle);
         root.getChildren().addAll(circle1, circle2, circle3, circle4, circle5);
@@ -104,48 +121,47 @@ public class fallingNotes extends Application{
         
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
-            public void handle(KeyEvent event) {
-                if (event.getCode() == KeyCode.SPACE){
-                	if (circle3.getCenterY() >= 560)
-                	{
-                		scenetitle.setText("" + score + "");   
-                		score = score + 100;
-                	}
-                	score = score + 0;
-                }          
+            public void handle(KeyEvent event) {      
                 if(event.getCode() == KeyCode.D) {
-                	if (circle1.getCenterY() >= 560)
-                	{
-                		scenetitle.setText("" + score + "");   
-                		score = score + 100;
-                	}
+                	getPoints(circle1);
+                	scenetitle.setText("" + score + "");   
                 }
                 if(event.getCode() == KeyCode.F) {
-                	if (circle2.getCenterY() >= 560)
-                	{
-                		scenetitle.setText("" + score + "");   
-                		score = score + 100;
-                	}
+                	getPoints(circle2);
+                	scenetitle.setText("" + score + "");  
                 }
+                if (event.getCode() == KeyCode.SPACE){
+                	getPoints(circle3);
+                	scenetitle.setText("" + score + "");   
+                }    
                 if(event.getCode() == KeyCode.J) {
-                	if (circle4.getCenterY() >= 560)
-                	{
-                		scenetitle.setText("" + score + "");   
-                		score = score + 100;
-                	}
+                	getPoints(circle4);
+                	scenetitle.setText("" + score + "");   
+
                 }
-                if(event.getCode() == KeyCode.K) {      
-                	if (circle5.getCenterY() >= 560)
-                	{
-                		scenetitle.setText("" + score + "");   
-                		score = score + 100;
-                	}
+                if(event.getCode() == KeyCode.K) {     
+                	getPoints(circle5);
+                	scenetitle.setText("" + score + "");   
                 }
             }
         });
 	}
 	
-	
+	public void getPoints(Circle dumb)
+	{
+		if (dumb.getCenterY() >= 585)
+		{
+			score = score + 300;
+		}
+		else if (dumb.getCenterY() >= 550)
+		{
+			score = score + 100;
+		}
+		else
+		{
+			score = score + 0;
+		}
+	}
 	
 	public static void main(String[] args) {
         Application.launch(args);    
